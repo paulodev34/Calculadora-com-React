7cfeaf9ad62f62a005a4f59b4240ab1416f11eda
Billy paulinho <paulo_neal@hotmail.com>
2022-08-22 18:16:27 -0300

    s

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import "./App.css";
+
+const App = () => {
+  const [resultado, setResultado] = useState("");
+  return (
+    <>
+      <div className="container">
+        <form>
+          <input type="text" value={resultado} />
+        </form>
+        <div className="teclado">
+          <button>AC</button>
+          <button>+/-</button>
+          <button>/</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>X</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+          <button>0</button>
+          <button>,</button>
+          <button>=</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
