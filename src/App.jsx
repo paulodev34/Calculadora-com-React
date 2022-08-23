2b12423d53a498b8de4637c9b1d946e51f7f4723
Billy paulinho <paulo_neal@hotmail.com>
2022-08-23 10:07:52 -0300

    botao de porcentagem

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
           <button>AC</button>
           <button>+/-</button>
           <button>/</button>
+          <button>%</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
