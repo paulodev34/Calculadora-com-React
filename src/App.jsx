45e4246abffebdd5f5f44395e7f6ff8a9d8b43fb
Billy paulinho <paulo_neal@hotmail.com>
2022-08-23 18:36:43 -0300

    novas cores

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = () => {
-  const [resultado] = useState("");
+  const [resultado, setResultado] = useState("");
+
+  const handleClick = (e) => {
+    setResultado(resultado.concat(e.target.name));
+  };
+
+  const AC = () => {
+    setResultado("");
+  };
   return (
     <>
       <div className="container">
@@ -10,25 +18,63 @@ const App = () => {
           <input type="text" value={resultado} />
         </form>
         <div className="teclado">
-          <button>AC</button>
-          <button>+/-</button>
-          <button>/</button>
-          <button>%</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>X</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>-</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>+</button>
-          <button>0</button>
-          <button>,</button>
-          <button>=</button>
+          <button className="botao-cinza" onClick={AC} id="AC">
+            C
+          </button>
+          <button className="botao-cinza" name="+/-" onClick={handleClick}>
+            +/-
+          </button>
+          <button className="botao-cinza" name="%" onClick={handleClick}>
+            %
+          </button>
+          <button className="botao-laranja" name="/" onClick={handleClick}>
+            /
+          </button>
+          <button name="7" onClick={handleClick}>
+            7
+          </button>
+          <button name="8" onClick={handleClick}>
+            8
+          </button>
+          <button name="9" onClick={handleClick}>
+            9
+          </button>
+          <button className="botao-laranja" name="X" onClick={handleClick}>
+            X
+          </button>
+          <button name="4" onClick={handleClick}>
+            4
+          </button>
+          <button name="5" onClick={handleClick}>
+            5
+          </button>
+          <button name="6" onClick={handleClick}>
+            6
+          </button>
+          <button className="botao-laranja" name="-" onClick={handleClick}>
+            -
+          </button>
+          <button name="1" onClick={handleClick}>
+            1
+          </button>
+          <button name="2" onClick={handleClick}>
+            2
+          </button>
+          <button name="3" onClick={handleClick}>
+            3
+          </button>
+          <button className="botao-laranja" name="+" onClick={handleClick}>
+            +
+          </button>
+          <button name="0" onClick={handleClick} id="zero">
+            0
+          </button>
+          <button name="," onClick={handleClick}>
+            ,
+          </button>
+          <button className="botao-laranja" onClick={handleClick}>
+            =
+          </button>
         </div>
       </div>
     </>
